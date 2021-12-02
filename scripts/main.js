bbf55afa30577fd118af52be1d6b78e3ea06c72b
unknown <Adegboroblessing@gmail.com>
2021-12-02 09:54:01 +0100

    my first javascript project

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,22 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
+document.querySelector('html') . onclick = function()
+    {alert('stop poking me ');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Mozilla_Firefox_3.5_logo_256.png') {
+      myImage.setAttribute('src','https://www.freeiconspng.com/thumbs/mozilla-firefox-icon/mozilla-firefox-icon-3.png');
+    } else {
+      myImage.setAttribute('src','https://upload.wikimedia.org/wikipedia/commons/e/e7/Mozilla_Firefox_3.5_logo_256.png');
+    }
+}
+
+
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
